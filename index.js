516eb379bbaf435a5f1bc97e0248f36d3e84b78b
Tehreem-1 <152809828+Tehreem-1@users.noreply.github.com>
2023-12-21 14:02:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly enter your first no: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly enter your second no: "
+    },
+    {
+        type: "list",
+        name: "Operator",
+        Choices: ["*", "+", "-", "/"],
+        message: "Select Operator:  "
+    },
+]);
+const { numberOne, numberTwo, Operator } = answers;
+if (numberOne && numberTwo && Operator) {
+    let result = 0;
+    if (Operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (Operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    if (Operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (Operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    if (Operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
